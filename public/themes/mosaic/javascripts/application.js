60126742299b1b3d73ecfd88e0f874737a8a9917
Brent Faulkner <brent.faulkner@mosaic.com>
2011-11-13 15:44:11 -0500

    mobile-friendly theme and view

--- public/themes/mosaic/javascripts/application.js
+++ public/themes/mosaic/javascripts/application.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+  window.scrollTo(0,1);
+
   $('a.login_submit').closest('form').find('input').keydown(function(e) {
     if (e.which == 13) {
       $(this).closest('form').submit();
