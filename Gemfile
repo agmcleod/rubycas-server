69b2b5e9a568e043301bef59b27744b7e6410a40
vagrant <vagrant@crumpet.vm>
2013-12-06 15:34:07 -0500

    added a sql attr encrypted authenticator

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source "http://rubygems.org"
 gem "activerecord", "~> 3.0.0"
 gem "activesupport", "~> 3.0.0"
+gem 'attr_encrypted', '~> 1.2.0'
 gem 'bcrypt-ruby', '~> 3.0.0'
 gem "sinatra", "~> 1.1.0"
 gem "gettext", "~> 2.1.0"
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -37,6 +37,8 @@ preload_app false
 GC.respond_to?(:copy_on_write_friendly=) and
   GC.copy_on_write_friendly = true
 
+check_client_connection false
+
 before_fork do |server, worker|
   # the following is highly recomended for Rails + "preload_app true"
   # as there's no need for the master process to hold a connection
